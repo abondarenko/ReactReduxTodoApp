a343d6d97b8b300360c3c99b041f7569c7cf8170
Alexander Bondarenko <a.bondarenko.box@gmail.com>
2018-09-25 00:28:37 +0300

    Add actions for adding/removing hobbies and movies

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -8,13 +8,53 @@ console.log('Starting redux example');
 // 3. No async
 // 4. No changed of input
 
-var reducer = (state = {name: 'Anonymous'}, action) => {
+var stateDefault = {
+    name: 'Anonymous',
+    hobbies: [],
+    movies: []
+};
+var nextHobbyId = 1;
+var nextMovieId = 1;
+var reducer = (state = stateDefault, action) => {
     // state = state || {name: 'Anonymous'};
     switch (action.type) {
         case 'CHANGE_NAME':
             return {
                 ...state,
                 name: action.name
+            };
+        case 'ADD_HOBBY':
+            return {
+                ...state,
+                hobbies: [
+                    ...state.hobbies,
+                    {
+                        id: nextHobbyId++,
+                        hobby: action.hobby
+                    }
+                ]
+            };
+        case 'REMOVE_HOBBY':
+            return {
+                ...state,
+                hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id)
+            };
+        case 'ADD_MOVIE':
+            return {
+                ...state,
+                movies: [
+                    ...state.movies,
+                    {
+                        id: nextMovieId++,
+                        movie: action.movie,
+                        genre: action.genre
+                    }
+                ]
+            };
+        case 'REMOVE_MOVIE':
+            return {
+                ...state,
+                movies: state.movies.filter((movie) => movie.id !== action.id)
             };
         default:
             return state;
@@ -30,6 +70,7 @@ var unsubscribe = store.subscribe(() => {
     var state = store.getState();
     console.log('Name is', state.name);
     document.getElementById('app').innerHTML = state.name;
+    console.log('New state', store.getState());
 });
 
 // unsubscribe();
@@ -42,6 +83,38 @@ store.dispatch({
 });
 
 store.dispatch({
+    type: 'ADD_HOBBY',
+    hobby: 'Running'
+});
+
+store.dispatch({
+    type: 'ADD_HOBBY',
+    hobby: 'Walking'
+});
+
+store.dispatch({
+    type: 'ADD_MOVIE',
+    movie: 'Gone in 60s',
+    genre: 'action'
+});
+
+store.dispatch({
+    type: 'ADD_MOVIE',
+    movie: 'Blair Witch',
+    genre: 'horror'
+});
+
+store.dispatch({
     type: 'CHANGE_NAME',
     name: 'Billy'
+});
+
+store.dispatch({
+    type: 'REMOVE_HOBBY',
+    id: 2
+});
+
+store.dispatch({
+    type: 'REMOVE_MOVIE',
+    id: 1
 });
